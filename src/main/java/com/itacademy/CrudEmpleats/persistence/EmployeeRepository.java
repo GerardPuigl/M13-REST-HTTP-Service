3fa40516d32496484302f242f2360efb9d586e87
Gerard <gerardpuig31@hotmail.com>
2021-03-15 21:29:48 +0100

    Repository creation with items

--- src/main/java/com/itacademy/CrudEmpleats/persistence/EmployeeRepository.java
+++ src/main/java/com/itacademy/CrudEmpleats/persistence/EmployeeRepository.java
@@ -1,10 +1,26 @@
 package com.itacademy.CrudEmpleats.persistence;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.stereotype.Service;
+
+import com.itacademy.CrudEmpleats.domain.Employee;
+
+@Service
 public class EmployeeRepository {
-	
+
 	/*
-	 * Els objectes seran persistidos únicament en memòria 
+	 * Els objectes seran persistidos únicament en memòria
 	 * 
 	 */
+
+	private ArrayList<Employee> repository = new ArrayList<>(
+			Arrays.asList(
+					new Employee("Juan", "Director de Projectes"),
+					new Employee("Gerard", "Programador"),
+					new Employee("Maria", "Administrativa")
+					)
+			);
 
 }
